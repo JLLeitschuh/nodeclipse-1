c8ae40511ec4e9e3038522b6c4325ca54a753c94
Paul Verest <paul.verest@live.com>
2014-02-28 10:04:14 +0800

    disable "--tea-pot-mode"

--- org.nodeclipse.ui/src/org/nodeclipse/ui/preferences/PreferenceInitializer.java
+++ org.nodeclipse.ui/src/org/nodeclipse/ui/preferences/PreferenceInitializer.java
@@ -29,7 +29,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		store.setDefault(PreferenceConstants.NODECLIPSE_CONSOLE_ENABLED, true);
 		
 		//store.setDefault(PreferenceConstants.NODE_OPTIONS, "--harmony");
-		store.setDefault(PreferenceConstants.NODE_APPLICATION_ARGUMENTS, "--tea-pot-mode");
+		//store.setDefault(PreferenceConstants.NODE_APPLICATION_ARGUMENTS, "--tea-pot-mode");
 		
 		store.setDefault(PreferenceConstants.NODE_ALLOW_MANY, true);
 		
