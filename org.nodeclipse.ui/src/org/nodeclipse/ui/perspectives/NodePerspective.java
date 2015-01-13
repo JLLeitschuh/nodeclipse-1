2db14f5d4f47e1a5fc68263bfbe65fec98497fb4
Paul Verest <paul.verest@live.com>
2015-01-13 11:23:52 +0800

    put TCF terminals first using NodePerspective.java (remove xml-based definitions)
    http://stackoverflow.com/questions/27436886/eclipse-plugin-dev-putting-a-view-first-in-a-stack-breaks-the-stack
    
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=454884

--- org.nodeclipse.ui/src/org/nodeclipse/ui/perspectives/NodePerspective.java
+++ org.nodeclipse.ui/src/org/nodeclipse/ui/perspectives/NodePerspective.java
@@ -4,7 +4,9 @@ import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.views.IViewRegistry;
 
 /**
  * This class is meant to serve as an example for how various contributions are
@@ -15,7 +17,16 @@ import org.eclipse.ui.console.IConsoleConstants;
 public class NodePerspective implements IPerspectiveFactory {
 
     public static final String ID = "org.nodeclipse.ui.perspectives.NodePerspective";
+
+    public static final String ID_TERMINALS_VIEW = "org.eclipse.tcf.te.ui.terminals.TerminalsView";
+    public static final String ID_MARKDOWN_VIEW = "winterwell.markdown.views.MarkdownPreview";
+    public static final String ID_GFM_VIEW = "code.satyagraha.gfm.viewer.views.GfmView";
+    public static final String ID_DEBUG_VIEW = "org.eclipse.debug.ui.DebugView";
+    public static final String ID_LOG_VIEW = "org.eclipse.pde.runtime.LogView";
+    
     private IPageLayout factory;
+
+	private IViewRegistry viewRegistry;
 
     public NodePerspective() {
         super();
@@ -23,28 +34,65 @@ public class NodePerspective implements IPerspectiveFactory {
 
     public void createInitialLayout(IPageLayout factory) {
         this.factory = factory;
+        this.viewRegistry = PlatformUI.getWorkbench().getViewRegistry();
+        
         addViews();
+        addViewShortcuts();
         addActionSets();
         addNewWizardShortcuts();
         addPerspectiveShortcuts();
-        addViewShortcuts();
     }
 
     private void addViews() {
         // Creates the overall folder layout.
-        // Note that each new Folder uses a percentage of the remaining
-        // EditorArea.
+        // Note that each new Folder uses a percentage of the remaining EditorArea.
 
         IFolderLayout left = factory.createFolder("left", IPageLayout.LEFT, 0.25f, factory.getEditorArea());
         left.addView(IPageLayout.ID_PROJECT_EXPLORER);
 
         IFolderLayout rightBottom = factory.createFolder("rightBottom", IPageLayout.BOTTOM, 0.75f, factory.getEditorArea());// NON-NLS-1
+        if (viewRegistry.find(ID_TERMINALS_VIEW) != null){
+        	rightBottom.addView(ID_TERMINALS_VIEW);        
+        }
         rightBottom.addView(IConsoleConstants.ID_CONSOLE_VIEW);
+        rightBottom.addView(ID_DEBUG_VIEW);
+        if (viewRegistry.find(ID_MARKDOWN_VIEW) != null){
+        	rightBottom.addView(ID_MARKDOWN_VIEW); 
+        }
+        if (viewRegistry.find(ID_GFM_VIEW) != null){
+        	rightBottom.addView(ID_GFM_VIEW);        
+        }
         rightBottom.addView(IPageLayout.ID_PROBLEM_VIEW);
+        if (viewRegistry.find(ID_LOG_VIEW) != null){
+        	rightBottom.addView(ID_LOG_VIEW);        
+        }
 
         IFolderLayout rightTopRight = factory.createFolder("rightTopRight", IPageLayout.RIGHT, 0.75f, factory.getEditorArea());
         rightTopRight.addView(IPageLayout.ID_OUTLINE);
         rightTopRight.addView(IPageLayout.ID_TASK_LIST);
+    }
+    
+    private void addViewShortcuts() {
+		factory.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
+		factory.addShowViewShortcut(IPageLayout.ID_PROP_SHEET);
+		factory.addShowViewShortcut(IPageLayout.ID_PROJECT_EXPLORER);
+		factory.addShowViewShortcut(IPageLayout.ID_NAVIGATE_ACTION_SET);
+		factory.addShowViewShortcut(IPageLayout.ID_OUTLINE);
+		factory.addShowViewShortcut(IPageLayout.ID_BOOKMARKS);
+    	 
+        factory.addShowViewShortcut("org.eclipse.team.ui.GenericHistoryView"); // NON-NLS-1
+        factory.addShowViewShortcut("org.eclipse.ui.views.ResourceNavigator"); // NON-NLS-1
+        factory.addShowViewShortcut("com.eclipserunner.views.RunnerView"); // NON-NLS-1 @since 0.8
+        factory.addShowViewShortcut("org.eclipse.angularjs.ui.views.AngularExplorerView");
+        
+        //as in rightBottom
+        factory.addShowViewShortcut(ID_TERMINALS_VIEW);
+        factory.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
+        factory.addShowViewShortcut(ID_DEBUG_VIEW);
+        factory.addShowViewShortcut(ID_MARKDOWN_VIEW);
+        factory.addShowViewShortcut(ID_GFM_VIEW);
+        factory.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+        factory.addShowViewShortcut(ID_LOG_VIEW);
     }
 
     private void addActionSets() {
@@ -82,21 +130,6 @@ public class NodePerspective implements IPerspectiveFactory {
         factory.addNewWizardShortcut("tern.eclipse.ide.tools.ui.wizards.webbrowser.NewOrionWizard");// NON-NLS-1
         // http://ternjs.net/doc/manual.html#plugins
         // https://github.com/angelozerr/tern.java/wiki/Tern-Toolings
-    }
-
-    private void addViewShortcuts() {
-		factory.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
-		factory.addShowViewShortcut(IPageLayout.ID_PROP_SHEET);
-		factory.addShowViewShortcut(IPageLayout.ID_PROJECT_EXPLORER);
-		factory.addShowViewShortcut(IPageLayout.ID_NAVIGATE_ACTION_SET);
-    	 
-        factory.addShowViewShortcut("org.eclipse.team.ui.GenericHistoryView"); // NON-NLS-1
-        factory.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
-        factory.addShowViewShortcut("org.eclipse.ui.views.ResourceNavigator"); // NON-NLS-1
-        factory.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
-        factory.addShowViewShortcut(IPageLayout.ID_OUTLINE);
-        factory.addShowViewShortcut(IPageLayout.ID_BOOKMARKS); //@since 0.8
-        factory.addShowViewShortcut("com.eclipserunner.views.RunnerView"); // NON-NLS-1 @since 0.8
     }
 
 }
